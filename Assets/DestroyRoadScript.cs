964fb1bf1b25bc66a3d74ba02f58532c11f239bb
CShepherdSAE <44623939+CShepherdSAE@users.noreply.github.com>
2020-05-05 11:52:37 +1000

    changed road prefab

---
+++ Assets/DestroyRoadScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRoadScript : MonoBehaviour
+{
+
+    public GameObject road;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+        {
+            Destroy(road);
+            Debug.Log("road destroyed");
+        }
+    }
+
+}
